2003174d8147b3e619f9f05214def591507afef1
donskov <donskovdmitriyv@gmail.com>
2018-07-28 22:47:53 +0300

    updated main shell

--- src/shells/main/manifest.js
+++ src/shells/main/manifest.js
@@ -1,13 +1,10 @@
 export default (app, props) => { // eslint-disable-line
   return Promise.resolve()
-    .then(() => Promise.all([
-      app.mountTemplate('main', 'loading'),
-      app.mountLang('main', 'en'),
-    ]))
+    .then(() => app.mountLang('main', 'en'))
     .then(() => { // eslint-disable-line
       return app.mountContainer('main', {
         type: 'INIT:FROM_SHELL',
-      }).then(() => app.unmountStyle('main', 'loading'));
+      });
     })
     .catch(err => (
       Promise.reject(err)
